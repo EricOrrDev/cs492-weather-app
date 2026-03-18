60962e49c3c30dc59b3e3be8499c41fc6f430453
Eric Orr <155666503+EricOrrDev@users.noreply.github.com>
2026-03-18 14:50:46 -0700

    minor edits to make the hour axis title not be covered by the android navigation bar

--- lib/widgets/forecast/forecast.dart
+++ lib/widgets/forecast/forecast.dart
@@ -18,6 +18,7 @@ class ForecastWidget extends StatelessWidget {
         const Expanded(
           child: ExcludeSemantics(child: DetailedForecast()),
         ),
+        const SizedBox(height: 40),
       ],
     );
   }
